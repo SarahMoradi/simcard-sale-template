20ba1d74177506ff5413e612d3a014125fbe9733
Moradi <s.moradi@asiatech.cloud>
2022-01-18 10:16:31 +0330

    Completed offer page

--- src/component/offers.js
+++ src/component/offers.js
@@ -1,10 +1,203 @@
 import React from "react";
 import DesktopMenu from "./desktopMenu";
+import { Row, Col, Card, CardBody, CardText, CardTitle, CardSubtitle, Button } from 'reactstrap'
+import styles from './SignUp.module.css'
 
 const offers = () => {
   return (
     <>
       <DesktopMenu />
+      <div className="d-flex justify-content-center my-4">
+        <h3>لیست محصولات</h3>
+      </div>
+      <div className="d-flex mt-2 mx-4">
+          <Col className="d-flex justify-content-center align-items-center" sm="12" md="3">
+           <div className="mx-3">
+                  <Card className={ styles.card_style }>
+                          <CardBody>
+                            <CardTitle tag="h5">
+                             سیم کارت 3 ماهه
+                            </CardTitle>
+                            <CardSubtitle
+                              className="mb-2 text-muted"
+                              tag="h6"
+                            >
+                              پیشنهاد ویژه
+                            </CardSubtitle>
+                            <CardText>
+                             فعالسازی مکالمات | فعالسازی تماس و پیامک های درون و برون شهری  | فعالسازی رومینگ بین الملل
+                            </CardText>
+                            <Button color="success">
+                              خرید
+                            </Button>
+                          </CardBody>
+                        </Card>
+             </div>
+          </Col>
+          <Col className="d-flex justify-content-center align-items-center" sm="12" md="3">
+          <div className="mx-3">
+                  <Card className={ styles.card_style }>
+                          <CardBody>
+                            <CardTitle tag="h5">
+                             سیم کارت 6 ماهه
+                            </CardTitle>
+                            <CardSubtitle
+                              className="mb-2 text-muted"
+                              tag="h6"
+                            >
+                              پیشنهاد ویژه
+                            </CardSubtitle>
+                            <CardText>
+                             فعالسازی مکالمات | فعالسازی تماس و پیامک های درون و برون شهری  | فعالسازی رومینگ بین الملل
+                            </CardText>
+                            <Button color="success">
+                            خرید
+                            </Button>
+                          </CardBody>
+                        </Card>
+             </div>
+          </Col>
+          <Col className="d-flex justify-content-center align-items-center" sm="12" md="3">
+          <div className="mx-3">
+                  <Card className={ styles.card_style }>
+                          <CardBody>
+                            <CardTitle tag="h5">
+                             سیم کارت یکساله
+                            </CardTitle>
+                            <CardSubtitle
+                              className="mb-2 text-muted"
+                              tag="h6"
+                            >
+                             پیشنهاد ویژه
+                            </CardSubtitle>
+                            <CardText>
+                             فعالسازی مکالمات | فعالسازی تماس و پیامک های درون و برون شهری  | فعالسازی رومینگ بین الملل
+                            </CardText>
+                            <Button color="success">
+                            خرید
+                            </Button>
+                          </CardBody>
+                        </Card>
+             </div>
+          </Col>
+          <Col className="d-flex justify-content-center align-items-center" sm="12" md="3">
+          <div className="mx-3">
+                  <Card className={ styles.card_style }>
+                          <CardBody>
+                            <CardTitle tag="h5">
+                             سیم کارت 18 ماهه
+                            </CardTitle>
+                            <CardSubtitle
+                              className="mb-2 text-muted"
+                              tag="h6"
+                            >
+                              پیشنهاد ویژه
+                            </CardSubtitle>
+                            <CardText>
+                             فعالسازی مکالمات | فعالسازی تماس و پیامک های درون و برون شهری  | فعالسازی رومینگ بین الملل
+                            </CardText>
+                            <Button color="success">
+                            خرید
+                            </Button>
+                          </CardBody>
+                        </Card>
+             </div>
+          </Col>
+          </div>
+          <div className="d-flex mt-5 mx-4">
+          <Col className="d-flex justify-content-center align-items-center" sm="12" md="3">
+           <div className="mx-3">
+                  <Card className={ styles.card_style }>
+                          <CardBody>
+                            <CardTitle tag="h5">
+                            سیم کارت 2 ساله
+                            </CardTitle>
+                            <CardSubtitle
+                              className="mb-2 text-muted"
+                              tag="h6"
+                            >
+                              پیشنهاد ویژه
+                            </CardSubtitle>
+                            <CardText>
+                             فعالسازی مکالمات | فعالسازی تماس و پیامک های درون و برون شهری  | فعالسازی رومینگ بین الملل
+                            </CardText>
+                            <Button color="success">
+                            خرید
+                            </Button>
+                          </CardBody>
+                        </Card>
+             </div>
+          </Col>
+          <Col className="d-flex justify-content-center align-items-center" sm="12" md="3">
+          <div className="mx-3">
+                  <Card className={ styles.card_style }>
+                          <CardBody>
+                            <CardTitle tag="h5">
+                             سیم کارت 5 ساله
+                            </CardTitle>
+                            <CardSubtitle
+                              className="mb-2 text-muted"
+                              tag="h6"
+                            >
+                             پیشنهاد ویژه
+                            </CardSubtitle>
+                            <CardText>
+                             فعالسازی مکالمات | فعالسازی تماس و پیامک های درون و برون شهری  | فعالسازی رومینگ بین الملل
+                            </CardText>
+                            <Button color="success">
+                            خرید
+                            </Button>
+                          </CardBody>
+                        </Card>
+             </div>
+          </Col>
+          <Col className="d-flex justify-content-center align-items-center" sm="12" md="3">
+          <div className="mx-3">
+                  <Card className={ styles.card_style }>
+                          <CardBody>
+                            <CardTitle tag="h5">
+                             سیم کارت 10 ساله
+                            </CardTitle>
+                            <CardSubtitle
+                              className="mb-2 text-muted"
+                              tag="h6"
+                            >
+                              پیشنهاد ویژه
+                            </CardSubtitle>
+                            <CardText>
+                             فعالسازی مکالمات | فعالسازی تماس و پیامک های درون و برون شهری  | فعالسازی رومینگ بین الملل
+                            </CardText>
+                            <Button color="success">
+                            خرید
+                            </Button>
+                          </CardBody>
+                        </Card>
+             </div>
+          </Col>
+          <Col className="d-flex justify-content-center align-items-center" sm="12" md="3">
+          <div className="mx-3">
+                  <Card className={ styles.card_style }>
+                          <CardBody>
+                            <CardTitle tag="h5">
+                            سیم کارت دائمی
+                            </CardTitle>
+                            <CardSubtitle
+                              className="mb-2 text-muted"
+                              tag="h6"
+                            >
+                              پیشنهاد ویژه
+                            </CardSubtitle>
+                            <CardText>
+                             فعالسازی مکالمات | فعالسازی تماس و پیامک های درون و برون شهری  | فعالسازی رومینگ بین الملل
+                            </CardText>
+                            <Button color="success">
+                            خرید
+                            </Button>
+                          </CardBody>
+                        </Card>
+             </div>
+          </Col>
+          </div>
     </>
   );
 };
